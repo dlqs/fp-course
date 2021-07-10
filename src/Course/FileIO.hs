3c450ba7e3c3842d9384cd379e8d7d2f6324178c
Donald Lee <dlqs@gmx.com>
2021-07-11 05:06:46 +0800

    iudpate

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,45 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile fp c = putStrLn fp >> putStrLn c
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles Nil = return ()
+printFiles (x:.xs) = printFile (fst x) (snd x) >> printFiles xs
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile fp = readFile fp >>= \f -> return (f, fp)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles Nil = return Nil
+getFiles (x:.xs) = do
+  f <- getFile x
+  fs <- getFiles xs
+  return (f:.fs)
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run fn = getFile fn >>= \(x, y) -> getFiles (lines y) >>= printFiles
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = join <$> getArgs >>= run
 
 ----
 
